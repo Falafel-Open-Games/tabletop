a4707ff9ff6408bbdfefe8c77287ed78e4e80156
Lucas Fonseca <lfonseca010@gmail.com>
2025-12-11 15:39:21 -0300

    Fix url params to run on a browser

--- src/main.gd
+++ src/main.gd
@@ -1,7 +1,7 @@
 extends Node
 
 func _ready():
-    if OS.has_feature("dedicated_server") or Arguments.get_cli_flag("server"):
+    if OS.has_feature("dedicated_server") or Arguments.get_cli_flag(&"server"):
         get_tree().change_scene_to_file("res://scenes/server.tscn")
     else:
         get_tree().change_scene_to_file("res://scenes/lobby.tscn")
