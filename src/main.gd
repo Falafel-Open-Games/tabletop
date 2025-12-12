92646f62936511d6fba8b5834ac2c9b983a80681
Lucas Fonseca <lfonseca010@gmail.com>
2025-12-12 15:37:42 -0300

    Fix parent node is busy error

--- src/main.gd
+++ src/main.gd
@@ -1,6 +1,9 @@
 extends Node
 
 func _ready():
+    call_deferred("_redirect")
+
+func _redirect():
     if OS.has_feature("dedicated_server") or Arguments.get_cli_flag(&"server"):
         get_tree().change_scene_to_file("res://scenes/server.tscn")
     else:
